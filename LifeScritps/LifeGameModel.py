087aa070cfd9e754db634669e24b9785f5721da0
fabian57fabian <fabian_57@yahoo.it>
2020-03-22 14:10:30 +0100

    Added main control bar

--- LifeScritps/LifeGameModel.py
+++ LifeScritps/LifeGameModel.py
@@ -7,7 +7,25 @@ class LifeGameModel(QObject):
 
     def __init__(self):
         super(LifeGameModel, self).__init__()
-        self.data_path = "../"
+        self.data_path = "Data"
+        self.icon_path = "icons"
+        self.icons_dataset = self.load_icons()
+
+    def load_icons(self):
+        icons_dts = {}
+        icons_dts['play'] = "Media-Controls-Play-icon.png"
+        icons_dts['pause'] = "Media-Controls-Pause-icon.png"
+        icons_dts['stop'] = "Media-Controls-Stop-icon.png"
+        icons_dts['open'] = "Folder-Open-icon.png"
+        icons_dts['save'] = "Save-icon.png"
+        return icons_dts
 
     def load_data(self):
         pass
+
+    def getIconPath(self, icon_name):
+        if icon_name in self.icons_dataset.keys():
+            path = os.path.join(self.icon_path, self.icons_dataset[icon_name])
+        else:
+            path = os.path.join(self.icon_path, "Error-Delete-Icon.png")
+        return path
--- LifeScritps/LifeGameView.py
+++ LifeScritps/LifeGameView.py
@@ -7,7 +7,7 @@ from LifeScritps.LifeGridWidget import LifeGridWidget
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QPushButton, QScrollArea, QLabel, QFormLayout, QMainWindow, \
-    QInputDialog, QLineEdit
+    QInputDialog, QLineEdit, QComboBox
 import os
 
 
@@ -28,5 +28,45 @@ class LifeGameView(QtWidgets.QWidget):
         main_window.setMaximumHeight(600)
         centralwidget = QtWidgets.QWidget(main_window)
         lay_vertical = QVBoxLayout()
+
+        lay_control = QHBoxLayout()
+        # TODO: set left alignment
+        lay_grid = QHBoxLayout()
+        lbl_grid_info = QLabel("Grid size:")
+        lay_grid.addWidget(lbl_grid_info)
+        cbox_select_grid = QComboBox(self)
+        sizes = ["100x100", "200x200", "300x300", "400x400", "500x500"]
+        cbox_select_grid.addItems(sizes)
+        for size in sizes:
+            cbox_select_grid.activated[str].connect(self.on_size_selected)
+        lay_grid.addWidget(cbox_select_grid)
+        lay_grid.setAlignment(Qt.AlignLeft)
+        lay_control.addLayout(lay_grid)
+
+        lay_playpause = QHBoxLayout()
+        btn_playpause = QPushButton()
+        btn_playpause.setIcon(QtGui.QIcon(self.model.getIconPath('play')))
+        btn_stop = QPushButton()
+        btn_stop.setIcon(QtGui.QIcon(self.model.getIconPath('stop')))
+        lay_playpause.addWidget(btn_playpause)
+        lay_playpause.addWidget(btn_stop)
+        lay_playpause.setAlignment(Qt.AlignCenter)
+        lay_control.addLayout(lay_playpause)
+
+        lay_manage_saved_sessions = QHBoxLayout()
+        btn_load_sess = QPushButton()
+        btn_load_sess.setIcon(QtGui.QIcon(self.model.getIconPath('open')))
+        btn_save_sess = QPushButton()
+        btn_save_sess.setIcon(QtGui.QIcon(self.model.getIconPath('save')))
+        lay_manage_saved_sessions.addWidget(btn_load_sess)
+        lay_manage_saved_sessions.addWidget(btn_save_sess)
+        lay_manage_saved_sessions.setAlignment(Qt.AlignRight)
+        lay_control.addLayout(lay_manage_saved_sessions)
+
+        lay_vertical.addLayout(lay_control)
+
         centralwidget.setLayout(lay_vertical)
         main_window.setCentralWidget(centralwidget)
+
+    def on_size_selected(self, selected_size):
+        pass
