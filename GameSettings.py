a8b64f3bbba7061e78183ba6c0362239221cf565
fabian57fabian <fabian_57@yahoo.it>
2020-03-22 23:38:31 +0100

    Changed default cells count

--- GameSettings.py
+++ GameSettings.py
@@ -1,6 +1,6 @@
 class LifeSettings(object):
     def __init__(self):
-        self.cells_h = 20
-        self.cells_w = 40
+        self.cells_h = 128
+        self.cells_w = 96
         self.selected_config = "clear"
         self.current_fps = 1.0
